80e86bb66c29f797b2f9686a948cadb003b9733e
Anna Volkova <annie_v@bk.ru>
2017-04-22 22:20:43 +0300

    delete src folder

--- src/index/index.js
+++
@@ -1,9 +0,0 @@
-const startGameButtons = $('.js-start-game');
-
-startGameButtons.on('click', createRoom);
-
-function createRoom(){
-  const url = `/room/${generateId(4)}`;
-  console.log(url);
-  window.location.href = url;
-}
